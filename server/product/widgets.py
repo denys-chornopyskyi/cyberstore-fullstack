f9f936f3de4eae86dec86f2446da260e82e5c71d
denys-chornopyskyi <chornopyskyidenys@gmail.com>
2025-11-02 14:30:03 +0100

    feat(widget): complete custom json widget

--- server/product/widgets.py
+++ server/product/widgets.py
@@ -1,10 +1,26 @@
 from django import forms
+from ast import literal_eval
+
+from django.http import QueryDict
+from django.utils.termcolors import color_names
+
 
 class JsonKeyValueWidget(forms.Widget):
     template_name = 'widgets/json_key_value_widget.html'
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        print(isinstance(value, dict))
+        if literal_eval(value):
+            context['widget']['pairs'] = [{'key': k, 'value': v} for k, v in literal_eval(value).items()]
+        else:
+            context['widget']['pairs'] = [{'key': '', 'value': ''}]
         return context
 
+    def value_from_datadict(self, data: QueryDict, files, name):
+        keys = data.getlist(f'{name}_key')
+        values = data.getlist(f'{name}_value')
+        print(keys)
+        print(values)
+        result = dict(zip(keys, values))
+        print(result)
+        return result
